f0c4c796f73e90f702e145bcd25ba95ff100fd21
Mitchell Brenner <mitchellbrenner@Mitchells-MacBook-Pro.local>
2025-02-18 16:13:11 -0700

    Added convex functions

---
+++ components/EventList.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+
+function EventList() {
+  const events = useQuery(api.events.get);
+
+  return <div>EventList</div>;
+}
+
+export default EventList;
